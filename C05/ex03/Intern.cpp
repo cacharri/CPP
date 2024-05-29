edfdcefbc753a2214b284860e133666d689eaff1
Inigo Alvarez echarri <ialvarez@c3r1s3.42madrid.com>
2024-05-29 19:51:18 +0200

    fixed

--- C05/ex03/Intern.cpp
+++ C05/ex03/Intern.cpp
@@ -10,68 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "Bureaucrat.hpp"
-#include "AForm.hpp"
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "PresidentialPardonForm.hpp"
-#include "Intern.hpp"
-
-Intern::Intern() {}
-
-Intern::~Intern() {}
-
-Intern::Intern(const Intern&) {
-}
-
-Intern& Intern::operator=(const Intern& other) {
-    if (this != &other) {}
-    return *this;
-}
-*/
-/*AForm* Intern::makeForm(const std::string& formName, const std::string& target) const {
-    if (formName == "presidential pardon") {
-        return new PresidentialPardonForm(target);
-    } else if (formName == "shrubbery creation") {
-        return new ShrubberyCreationForm(target);
-    } else if (formName == "robotomy request") {
-        return new RobotomyRequestForm(target);
-    } else {
-        std::cerr << "Error: Form " << formName << " does not exist" << std::endl;
-        return NULL;
-    }
-}*/
-/*AForm* Intern::makeForm(const std::string& formName, const std::string& target) const {
-    // Definición de la estructura FormType
-    struct FormType {
-        const char* name;
-        AForm* (*create)(const std::string& target);
-    };
-
-    // Inicialización del arreglo forms
-    const FormType forms[] = {
-        {"presidential pardon", PresidentialPardonForm},
-        {"shrubbery creation", ShrubberyCreationForm},
-        {"robotomy request", RobotomyRequestForm},
-    };
-
-    const size_t formsCount = sizeof(forms) / sizeof(forms[0]);
-
-    for (size_t i = 0; i < formsCount; ++i) {
-        if (formName == forms[i].name) {
-            std::cout << "Intern creates " << formName << std::endl;
-            return forms[i].create(target);
-        }
-    }
-
-    std::cerr << "Error: Form " << formName << " does not exist" << std::endl;
-    return NULL;
-}*/
 #include "Intern.hpp"
 #include <iostream>
 #include <string>
 
-// Declaraciones de las funciones creadoras
 AForm* createPresidentialPardonForm(const std::string& target);
 AForm* createShrubberyCreationForm(const std::string& target);
 AForm* createRobotomyRequestForm(const std::string& target);
@@ -86,12 +28,10 @@ Intern::Intern(const Intern& other) {
 
 Intern& Intern::operator=(const Intern& other) {
     if (this != &other) {
-        // Asignar atributos si los tuviera
     }
     return *this;
 }
 
-// Definición de las funciones creadoras
 AForm* createPresidentialPardonForm(const std::string& target) {
     return new PresidentialPardonForm(target);
 }
@@ -105,13 +45,11 @@ AForm* createRobotomyRequestForm(const std::string& target) {
 }
 
 AForm* Intern::makeForm(const std::string& formName, const std::string& target) const {
-    // Definición de la estructura FormType
     struct FormType {
         const char* name;
         AForm* (*create)(const std::string& target);
     };
 
-    // Inicialización del arreglo forms
     const FormType forms[] = {
         {"presidential pardon", createPresidentialPardonForm},
         {"shrubbery creation", createShrubberyCreationForm},
